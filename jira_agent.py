be65d15669d360d5b0bf18131ed9b8da38677d23
Evan Lin <evan.if.lin@linecorp.com>
2023-09-17 18:33:59 +0800

    feat: add assignee and status fields to Jira search functionality
    - Added `assignee` field to the `SearchIssueInput` class
    - Added `status` field to the `SearchIssueInput` class
    - Modified the `_run` method in the `JiraSearchTool` class to include `assignee` and `status` parameters
    - Modified the `_arun` method in the `JiraSearchTool` class to include `assignee` and `status` parameters
    - Modified the `search_jira_issue` function to include `assignee` and `status` parameters
    - Modified the JQL query in the `search_jira_issue` function to include `assignee` and `status` conditions based on the provided parameters in the function call
    - Modified the issue_list dictionary in the `search_jira_issue` function to include the `assignee` and `status` fields
    - Updated the main function to call the `search_jira_issue` function with different parameters
    
    Signed-off-by: Evan Lin <evan.if.lin@linecorp.com>

--- jira_agent.py
+++ jira_agent.py
@@ -13,21 +13,23 @@ class SearchIssueInput(BaseModel):
     """Search Jira issue input parameters."""
     # project_key: str = Field(..., description="The key of the project")
     issue_title: str = Field(..., description="The title of the issue")
+    assignee: str = Field(..., description="The assignee of the issue")
+    status: str = Field(..., description="The status of the issue, e.g. OPEN, IN PROGRESS, DONE. Not-closed issues will be treat as OPEN and IN PROGRESS .")
 
 class JiraSearchTool(BaseTool):
     name = "search_jira_issue"
     description = "Search issues in Jira"
 
-    def _run(self, issue_title:str):
-        issue_results = search_jira_issue(issue_title)
+    def _run(self, issue_title:str, assignee: str, status: str):
+        issue_results = search_jira_issue(issue_title, assignee, status)
         return issue_results
 
-    def _arun(self, issue_title:str):
+    def _arun(self, issue_title:str, assignee: str, status: str):
         raise NotImplementedError("This tool does not support async")
 
     args_schema: Optional[Type[BaseModel]] = SearchIssueInput
 
-def search_jira_issue(issue_title=None):
+def search_jira_issue(issue_title=None, assignee=None, status=None):
     print(issue_title)
     # 建立 JIRA 連線
     try:
@@ -37,7 +39,17 @@ def search_jira_issue(issue_title=None):
 
     # 建立 JQL 查詢語句
     jql_str = f'project = DEVRELTW'
-    jql_str += f' AND summary ~ "{issue_title}"'
+    if issue_title:
+        jql_str += f' AND summary ~ "{issue_title}"'
+    if assignee:
+        jql_str += f' AND assignee = "{assignee}"'
+    if status:
+        status_list = status.split(',')
+        status_list = [s.strip() for s in status_list]  # remove leading and trailing spaces
+        # Search status in a list of status_list
+        jql_str += ' AND status IN ({})'.format(", ".join(f'"{s}"' for s in status_list))
+        # jql_str += f' AND status IN ({", ".join(f"\"{s}\"" for s in status_list)})'
+        #jql_str += f' AND status IN ({", ".join(f'"{s}"' for s in status_list)})'
 
     # 搜尋 issue
     try:
@@ -46,18 +58,21 @@ def search_jira_issue(issue_title=None):
             issue_list.append({
                 "issue_key": singleIssue.key,
                 "summary": singleIssue.fields.summary,
-                "reporter": singleIssue.fields.reporter.displayName
+                "reporter": singleIssue.fields.reporter.displayName,
+                "assignee": singleIssue.fields.assignee.displayName if singleIssue.fields.assignee else None,
+                "status": singleIssue.fields.status.name
             })
         return {"status": "success", "issues": issue_list}
     except Exception as e:
         return {"status": "failure", "reason": str(e)}
+    
 
 def main():
     # 使用範例
     # search_data = SearchIssueInput(issue_title="Blog")
 
     # tool = JiraSearchTool()
-    response = search_jira_issue("Blog")
+    response = search_jira_issue("清大", assignee=None, status="OPEN, IN PROGRESS")
     print(response)
 
 if __name__ == "__main__":
